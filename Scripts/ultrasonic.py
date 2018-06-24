0f387a5a249c51c05b89c6b1450c70f1089c409d
TheoWestny <thewe260@student.liu.se>
2018-06-24 20:20:23 +0200

    Create ultrasonic.py

---
+++ Scripts/ultrasonic.py
@@ -0,0 +1,43 @@
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
+
+
+class Ultrasonic:
+    def __init__(self, echo,  *trig):
+        self.trigger1 = trig[0]
+        self.echo = echo
+        self.__next__()
+
+    def __next__(self):
+        GPIO.setup(self.echo, GPIO.IN)
+        GPIO.setup(self.trigger1, GPIO.OUT)
+
+        GPIO.output(self.trigger1, 0)
+
+    def distance(self, channel):
+        GPIO.output(channel, 1)
+        time.sleep(0.00001)
+        GPIO.output(channel, 0)
+        while GPIO.input(self.echo) == 0:
+            pulse_start = time.time()
+        while GPIO.input(self.echo) == 1:
+            pulse_end = time.time()
+        pulse_duration = pulse_end - pulse_start
+        distance = pulse_duration * 17150
+        distance = round(distance, 2)
+        return distance
+
+
+def main():
+    class1 = Ultrasonic(24, 23)
+    while True:
+        print(class1.distance(23))
+        time.sleep(1)
+
+
+try:
+    main()
+except KeyboardInterrupt:
+    GPIO.cleanup()
+    print("Done!")
