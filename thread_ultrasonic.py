388c667d840e4a4e37bb47d469281b3b6b33d68d
TheoWestny <thewe260@student.liu.se>
2018-07-01 19:41:35 +0200

    Update thread_ultrasonic.py

--- thread_ultrasonic.py
+++ thread_ultrasonic.py
@@ -1,37 +1,47 @@
 import RPi.GPIO as GPIO
 import time
 import threading
-
 GPIO.setmode(GPIO.BCM)
 
-class Ultrasonic_thread(threading.thread):
-  pass
 
-print "Distance Measurement In Progress"
+class Ultrasonic(threading.Thread):
+    def __init__(self, name, trig, echo):
+        threading.Thread.__init__(self)
+        self.name = name
+        self.trig_pin = trig
+        self.echo_pin = echo
 
-GPIO.setup(TRIG,GPIO.OUT)
-GPIO.setup(ECHO,GPIO.IN)
+        GPIO.setup(self.trig_pin, GPIO.OUT)
+        GPIO.setup(self.echo_pin, GPIO.IN)
 
-GPIO.output(TRIG, False)
-print "Waiting For Sensor To Settle"
-time.sleep(2)
+    def distance(self):
+        GPIO.output(self.trig_pin, 1)
+        time.sleep(0.00001)
+        GPIO.output(self.trig_pin, 0)
+        while GPIO.input(self.echo_pin) == 0:
+            pulse_start = time.time()
+        while GPIO.input(self.echo_pin) == 1:
+            pulse_end = time.time()
+        pulse_duration = pulse_end - pulse_start
+        distance = pulse_duration * 17150
+        distance = round(distance, 2)
+        return distance
 
-GPIO.output(TRIG, True)
-time.sleep(0.00001)
-GPIO.output(TRIG, False)
+    def run(self):
+        distance = self.distance()
+        print(str(self.name) + " Distance: " + str(distance) + " cm \n")
 
-while GPIO.input(ECHO)==0:
-  pulse_start = time.time()
 
-while GPIO.input(ECHO)==1:
-  pulse_end = time.time()
+def main():
+    class1 = Ultrasonic("App1", 5, 16)
+    class1.start()
 
-pulse_duration = pulse_end - pulse_start
 
-distance = pulse_duration * 17150
+try:
+    main()
+    GPIO.cleanup()
+    print("Done!")
+except KeyboardInterrupt:
+    GPIO.cleanup()
+    print("Done!")
 
-distance = round(distance, 2)
-
-print "Distance:",distance,"cm"
-
-GPIO.cleanup()
